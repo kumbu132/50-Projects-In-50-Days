81c4ac6b70e4e21f49cd3c41ea3a4fd2cc9c2722
Kumbukani Kamanga <152120171097@ogrenci.ogu.edu.tr>
2021-02-12 22:35:51 +0200

    random choice picker

--- 13. Random Choice Picker/Random-Choice-Picker/public/assets/js/script.js
+++ 13. Random Choice Picker/Random-Choice-Picker/public/assets/js/script.js
@@ -7,33 +7,61 @@ let tagsString;
 
 textarea.focus();
 
-textarea.addEventListener("keydown", (e) => {
-  if (e.code === "Enter") {
-    for (let index = 0; index < 100000; index++) {
-      for (let i = 0; i < tags.length; i++) {
-        tags[i].classList.add("highlight");
-        tags[i].classList.remove("highlight");
-        console.log(index);
-      }
-    }
-    //highlight animation
-  }
-});
-
 textarea.addEventListener("keyup", (e) => {
-  if (e.code === "Comma") {
-    tagsString = "";
-    //add spans for tag with the inner HTML with the word that's just been typed
-    //const tags array = parse textarea.value by the comma and or map it to the tags array.
-    //tagsEl.innerHTML = tagsArray;
-
-    tagsArray = textarea.value.split(",").filter(function (el) {
-      return el.length != 0;
-    });
-
-    tagsArray.forEach((tag) => {
-      tagsString += ` <span class="tag">${tag}</span>`;
-    });
-    tagsEl.innerHTML = tagsString;
+  createTags(e.target.value);
+  if (e.code === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+    randomSelect();
   }
 });
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.append(tagEl);
+  });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highlightTag(randomTag);
+    setTimeout(() => {
+      unhighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unhighlightTag(tag) {
+  tag.classList.remove("highlight");
+}
