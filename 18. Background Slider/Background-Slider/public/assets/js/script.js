1dd1ff588a69f357dad873f31044ff0503ee5960
Kumbukani Kamanga <152120171097@ogrenci.ogu.edu.tr>
2021-02-18 20:07:10 +0200

    background-slider

---
+++ 18. Background Slider/Background-Slider/public/assets/js/script.js
@@ -0,0 +1,33 @@
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+let activeSlide = 0;
+
+setBgToBody();
+setActiveSlide();
+
+leftBtn.addEventListener('click', () => {
+  activeSlide > 0 ? activeSlide-- : (activeSlide = slides.length - 1);
+  setBgToBody();
+  setActiveSlide();
+});
+
+rightBtn.addEventListener('click', () => {
+  activeSlide < slides.length - 1 ? activeSlide++ : (activeSlide = 0);
+  setBgToBody();
+  setActiveSlide();
+});
+
+setBgToBody();
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove('active'));
+
+  slides[activeSlide].classList.add('active');
+}
